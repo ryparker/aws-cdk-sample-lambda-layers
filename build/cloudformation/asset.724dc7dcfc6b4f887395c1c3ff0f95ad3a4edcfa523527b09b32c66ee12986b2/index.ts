3c79d7e6c3a6c617e581bbb81d5a00e5b87f7cef
Ryan Parker <ryan.parker3@outlook.com>
2021-09-10 10:38:52 -0700

    Remove build dir

--- build/cloudformation/asset.724dc7dcfc6b4f887395c1c3ff0f95ad3a4edcfa523527b09b32c66ee12986b2/index.ts
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk';
-
-exports.handler = (event: Record<string, any>, context: Record<string, any>, callback: Function) => {
-  console.log(chalk.green('Hello World!'));
-  console.info(chalk.blue(`event: ${JSON.stringify(event)}`));
-};
--- build/cloudformation/asset.84187e2b9bad8313e406e18d52d8d6f6e08ddffae594772cc8ed673cd134ca1d/index.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk');
-
-exports.handler = (event, context, callback) => {
-  console.log(chalk.green('Hello World!'));
-  console.info(chalk.blue(`event: ${JSON.stringify(event)}`));
-};
